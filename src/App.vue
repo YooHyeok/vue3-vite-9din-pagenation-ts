88f34215743ff23f6c049dfbbecb7cf5572b790a
webdevyoo <yjk7454@naver.com>
2025-05-28 00:06:51 +0900

    feat: sass 테스트를 위한 scoped style 테스트 css 적용

--- src/App.vue
+++ src/App.vue
@@ -2,4 +2,16 @@
   <div>TEST</div>
 </template>
 <script setup lang="ts"></script>
-<style scoped></style>
+<style scoped>
+.page {
+  display: flex;
+  align-items: center;
+  justify-items: center;
+
+  width: 100%;
+  height: 100vh;
+
+  background-color: black;
+  color: white;
+}
+</style>
