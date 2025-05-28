1392d7cf4775307d77aee7f085e348b8ff37255c
webdevyoo <yjk7454@naver.com>
2025-05-29 00:47:08 +0900

    feat: Vue Entry Point인 main.ts에 route 전역 등록

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/styles/main.scss'
-createApp(App).mount('#app')
+import router from './routes/index'
+createApp(App).use(router).mount('#app')
