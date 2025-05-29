05c82d0bb6f1aa2f182cb8324b5039b7644cd370
webdevyoo <yjk7454@naver.com>
2025-05-30 00:08:19 +0900

    chore: `vite.config.ts` - 잔여 alias 설정

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,12 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@apis': fileURLToPath(new URL('./src/apis', import.meta.url)),
+      '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+      '@pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
+      '@routes': fileURLToPath(new URL('./src/routes', import.meta.url)),
+      '@store': fileURLToPath(new URL('./src/store', import.meta.url)),
     }
   },
   css: {
