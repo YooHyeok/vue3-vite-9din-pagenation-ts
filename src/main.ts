61820d15eb253ed376c1dcaaac2154a18d417bf9
webdevyoo <yjk7454@naver.com>
2025-05-30 00:09:16 +0900

    chore: `main.ts` - scss 전역 설정 후 엔트리 포인트 모듈에서 import 제거

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './assets/styles/main.scss'
+// import './assets/styles/main.scss'
 import router from './routes/index'
 createApp(App).use(router).mount('#app')
