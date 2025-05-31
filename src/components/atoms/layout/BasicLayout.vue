fba68008aaf671fb2874c7e2ddc49b2dea4907ca
webdevyoo <yjk7454@naver.com>
2025-06-01 02:19:22 +0900

    feat: `기본 레이아웃 구성` - BasicLayout.vue컴포넌트 구성 (slot)

---
+++ src/components/atoms/layout/BasicLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="basic-layout">
+    <slot></slot><!-- slot: BasicLayout 컴포넌트에 무엇이든 담길 수 있는 주머니 역할 -->
+  </div>
+</template>
+<script setup lang="ts"></script>
+<style lang="scss" scoped>
+.basic-layout {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  padding: 20px;
+  
+  background-color: $color-white-000;
+  border-radius: 24px;
+}
+</style>
