9f3c792e65f2e8f1ce56140e70e2d914a6bc341b
webdevyoo <yjk7454@naver.com>
2025-05-31 21:53:35 +0900

    feat: 설정 및 구성한 scss기반 전체 컴포넌트 style 적용

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,9 @@
 <template>
-  <div>TEST</div>
   <router-view/>
 </template>
 <script setup lang="ts"></script>
-<style scoped>
-.page {
-  display: flex;
-  align-items: center;
-  justify-items: center;
-
-  width: 100%;
-  height: 100vh;
-
-  background-color: black;
-  color: white;
+<style>
+body {
+  margin: 0px;
 }
 </style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,18 @@
 <template>
-  <div>INDEX.VUE 페이지</div>
+  <div class="page"></div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.page{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  height: 100vh;
+
+  background-color: $color-black-900;
+}
+</style>
