bd5dfe36c4c156929db9c1be86786d0d2b3f2441
webdevyoo <yjk7454@naver.com>
2025-06-05 01:10:06 +0900

    feat: 리스트 데이터 페이징처리 및 페이지네이션 구현

--- src/components/mocules/common/Pagination.vue
+++ src/components/mocules/common/Pagination.vue
@@ -3,13 +3,66 @@
     <button class="pagination__button" @click="moveToPrev">
       <img src="@assets/icons/arrow-left.svg" alt="">
     </button>
-    <button class="pagination__button" @click="moveToPrev"></button>
+    <button 
+      v-for="index in pages[step]" 
+      :key="index" 
+      class="pagination__button" 
+      @click="movePage(index)"
+      :class="{ active: index === page }"
+    >
+      {{ index }}
+    </button>
     <button class="pagination__button" @click="moveToNext">
       <img src="@assets/icons/arrow-right.svg" alt="">
     </button>
   </div>
 </template>
 <script setup lang="ts">
+import { computed, ref, toRef, toRefs } from 'vue';
+
+interface Props {
+  totalPage: number
+}
+const props = defineProps<Props>();
+const { totalPage } = toRefs(props);
+
+// 한 페이지당 리스트 개수 10개로 쪼개기
+const step = ref<number>(0);
+const pages = computed(() => {
+  const newArr: number[] = new Array()
+  for (let i = 1; i <= totalPage.value; i++) {
+    newArr.push(i)
+  }
+  const length = newArr.length;
+  const divide = Math.floor(length / 10) + (Math.floor(length % 10) > 0 ? 1 : 0);
+  const res = new Array();
+
+  for (let i = 0; i <= divide; i++) {
+    // newArr 배열에서 0부터 n개씩 잘라 반환할 res 배열에 push
+    res.push(newArr.splice(0, 10))
+  }
+  return res;
+})
+
+const page = ref<number>(1)
+const emit = defineEmits(['send-event'])
+const movePage = (pageIdx: number) => {
+  emit("send-event", pageIdx);
+  page.value = pageIdx;
+}
+const moveToPrev = () => {
+  if (page.value <= 1) return;
+  page.value --;
+  
+  emit("send-event", page.value);
+}
+const moveToNext = () => {
+  if (page.value < pages.value.length) { 
+    page.value ++;
+  } else return;
+  
+  emit("send-event", page.value);
+}
 
 </script>
 <style lang="scss" scoped>
--- src/components/organisms/Notice.vue
+++ src/components/organisms/Notice.vue
@@ -32,13 +32,14 @@
       </div>
     </div>
     <div class="notice__footer">
-      <pagination />
+      <pagination :totalPage="totalPage" @send-event="reset"/>
     </div>
   </div>
 </template>
 <script setup lang="ts">
 import NoticeList from '@components/mocules/List.vue'
-import { ref } from 'vue';
+import pagination from '@components/mocules/common/pagination.vue'
+import { onMounted, ref, watch } from 'vue';
 
 interface List {
   userName: string;
@@ -48,121 +49,302 @@ interface List {
   userSkill: string;
 }
 
-const tableData = ref<List[]>([
-  {
-      userName: 'Marco',
-      userRole: 'FrontEnd Developer',
-      userResumeScore: 89,
-      userSkillMatch: 80,
-      userSkill: 'Junior',
-  },
-  {
-      userName: 'Jeff',
-      userRole: 'BackEnd Developer',
-      userResumeScore: 72,
-      userSkillMatch: 65,
-      userSkill: 'Senior',
-  },
-  {
-      userName: 'Eric',
-      userRole: 'User Experience',
-      userResumeScore: 90,
-      userSkillMatch: 91,
-      userSkill: 'Senior',
-  },
-  {
-      userName: 'Merry',
-      userRole: 'User Interface',
-      userResumeScore: 56,
-      userSkillMatch: 43,
-      userSkill: 'Junior',
-  },
-  {
-      userName: 'Harry',
-      userRole: 'Project Manager',
-      userResumeScore: 61,
-      userSkillMatch: 81,
-      userSkill: 'Junior',
-  },
-  {
-      userName: 'John',
-      userRole: 'Project Orner',
-      userResumeScore: 100,
-      userSkillMatch: 100,
-      userSkill: 'Senior',
-  },
-  {
-      userName: 'Maxwell',
-      userRole: 'FrontEnd Developer',
-      userResumeScore: 50,
-      userSkillMatch: 60,
-      userSkill: 'Senior',
-  },
-  {
-      userName: 'Pole',
-      userRole: 'BackEnd Developer',
-      userResumeScore: 80,
-      userSkillMatch: 76,
-      userSkill: 'Senior',
-  },
-  //
-  {
-      userName: 'Marco',
-      userRole: 'FrontEnd Developer',
-      userResumeScore: 89,
-      userSkillMatch: 80,
-      userSkill: 'Junior',
-  },
-  {
-      userName: 'Jeff',
-      userRole: 'BackEnd Developer',
-      userResumeScore: 72,
-      userSkillMatch: 65,
-      userSkill: 'Senior',
-  },
-  {
-      userName: 'Eric',
-      userRole: 'User Experience',
-      userResumeScore: 90,
-      userSkillMatch: 91,
-      userSkill: 'Senior',
-  },
-  {
-      userName: 'Merry',
-      userRole: 'User Interface',
-      userResumeScore: 56,
-      userSkillMatch: 43,
-      userSkill: 'Junior',
-  },
-  {
-      userName: 'Harry',
-      userRole: 'Project Manager',
-      userResumeScore: 61,
-      userSkillMatch: 81,
-      userSkill: 'Junior',
-  },
-  {
-      userName: 'John',
-      userRole: 'Project Orner',
-      userResumeScore: 100,
-      userSkillMatch: 100,
-      userSkill: 'Senior',
-  },
-  {
-      userName: 'Maxwell',
-      userRole: 'FrontEnd Developer',
-      userResumeScore: 50,
-      userSkillMatch: 60,
-      userSkill: 'Senior',
-  },
-  {
-      userName: 'Pole',
-      userRole: 'BackEnd Developer',
-      userResumeScore: 80,
-      userSkillMatch: 76,
-      userSkill: 'Senior',
-  },
-])
+const tableData = ref<List[]>([]);
+const selectedPage = ref<number>(1); // 0페이지라는 UI는 없으므로 초기값을 1로 세팅
+const totalPage = ref<number>(0); // tableData의 개수에 따라 페이지네이션 UI에 그려지는 숫자리스트.
+const totalCount = ref<number | undefined>(); // 총 데이터 리스트 개수
+const limit = ref<number>(10); // 한 페이지당 나타낼 데이터 개수
+
+/* 컴포넌트 DOM이 렌더링 된 이후 실행 */
+onMounted(() => {
+  getData();
+})
+
+const getData = () => {
+  tableData.value = [
+    {
+        userName: 'Marco',
+        userRole: 'FrontEnd Developer',
+        userResumeScore: 89,
+        userSkillMatch: 80,
+        userSkill: 'Junior',
+    },
+    {
+        userName: 'Jeff',
+        userRole: 'BackEnd Developer',
+        userResumeScore: 72,
+        userSkillMatch: 65,
+        userSkill: 'Senior',
+    },
+    {
+        userName: 'Eric',
+        userRole: 'User Experience',
+        userResumeScore: 90,
+        userSkillMatch: 91,
+        userSkill: 'Senior',
+    },
+    {
+        userName: 'Merry',
+        userRole: 'User Interface',
+        userResumeScore: 56,
+        userSkillMatch: 43,
+        userSkill: 'Junior',
+    },
+    {
+        userName: 'Harry',
+        userRole: 'Project Manager',
+        userResumeScore: 61,
+        userSkillMatch: 81,
+        userSkill: 'Junior',
+    },
+    {
+        userName: 'John',
+        userRole: 'Project Orner',
+        userResumeScore: 100,
+        userSkillMatch: 100,
+        userSkill: 'Senior',
+    },
+    {
+        userName: 'Maxwell',
+        userRole: 'FrontEnd Developer',
+        userResumeScore: 50,
+        userSkillMatch: 60,
+        userSkill: 'Senior',
+    },
+    {
+        userName: 'Pole',
+        userRole: 'BackEnd Developer',
+        userResumeScore: 80,
+        userSkillMatch: 76,
+        userSkill: 'Senior',
+    },
+    {
+        userName: 'Marco',
+        userRole: 'FrontEnd Developer',
+        userResumeScore: 89,
+        userSkillMatch: 80,
+        userSkill: 'Junior',
+    },
+    {
+        userName: 'Jeff',
+        userRole: 'BackEnd Developer',
+        userResumeScore: 72,
+        userSkillMatch: 65,
+        userSkill: 'Senior',
+    },
+    {
+        userName: 'Eric',
+        userRole: 'User Experience',
+        userResumeScore: 90,
+        userSkillMatch: 91,
+        userSkill: 'Senior',
+    },
+    {
+        userName: 'Merry',
+        userRole: 'User Interface',
+        userResumeScore: 56,
+        userSkillMatch: 43,
+        userSkill: 'Junior',
+    },
+    {
+        userName: 'Harry',
+        userRole: 'Project Manager',
+        userResumeScore: 61,
+        userSkillMatch: 81,
+        userSkill: 'Junior',
+    },
+    {
+        userName: 'John',
+        userRole: 'Project Orner',
+        userResumeScore: 100,
+        userSkillMatch: 100,
+        userSkill: 'Senior',
+    },
+    {
+        userName: 'Maxwell',
+        userRole: 'FrontEnd Developer',
+        userResumeScore: 50,
+        userSkillMatch: 60,
+        userSkill: 'Senior',
+    },
+    {
+        userName: 'Pole',
+        userRole: 'BackEnd Developer',
+        userResumeScore: 80,
+        userSkillMatch: 76,
+        userSkill: 'Senior',
+    },
+    {
+        userName: 'Marco',
+        userRole: 'FrontEnd Developer',
+        userResumeScore: 89,
+        userSkillMatch: 80,
+        userSkill: 'Junior',
+    },
+    {
+        userName: 'Jeff',
+        userRole: 'BackEnd Developer',
+        userResumeScore: 72,
+        userSkillMatch: 65,
+        userSkill: 'Senior',
+    },
+    {
+        userName: 'Eric',
+        userRole: 'User Experience',
+        userResumeScore: 90,
+        userSkillMatch: 91,
+        userSkill: 'Senior',
+    },
+    {
+        userName: 'Merry',
+        userRole: 'User Interface',
+        userResumeScore: 56,
+        userSkillMatch: 43,
+        userSkill: 'Junior',
+    },
+    {
+        userName: 'Harry',
+        userRole: 'Project Manager',
+        userResumeScore: 61,
+        userSkillMatch: 81,
+        userSkill: 'Junior',
+    },
+    {
+        userName: 'John',
+        userRole: 'Project Orner',
+        userResumeScore: 100,
+        userSkillMatch: 100,
+        userSkill: 'Senior',
+    },
+    {
+        userName: 'Maxwell',
+        userRole: 'FrontEnd Developer',
+        userResumeScore: 50,
+        userSkillMatch: 60,
+        userSkill: 'Senior',
+    },
+    {
+        userName: 'Pole',
+        userRole: 'BackEnd Developer',
+        userResumeScore: 80,
+        userSkillMatch: 76,
+        userSkill: 'Senior',
+    },
+    {
+        userName: 'Marco',
+        userRole: 'FrontEnd Developer',
+        userResumeScore: 89,
+        userSkillMatch: 80,
+        userSkill: 'Junior',
+    },
+    {
+        userName: 'Jeff',
+        userRole: 'BackEnd Developer',
+        userResumeScore: 72,
+        userSkillMatch: 65,
+        userSkill: 'Senior',
+    },
+    {
+        userName: 'Eric',
+        userRole: 'User Experience',
+        userResumeScore: 90,
+        userSkillMatch: 91,
+        userSkill: 'Senior',
+    },
+    {
+        userName: 'Merry',
+        userRole: 'User Interface',
+        userResumeScore: 56,
+        userSkillMatch: 43,
+        userSkill: 'Junior',
+    },
+    {
+        userName: 'Harry',
+        userRole: 'Project Manager',
+        userResumeScore: 61,
+        userSkillMatch: 81,
+        userSkill: 'Junior',
+    },
+    {
+        userName: 'John',
+        userRole: 'Project Orner',
+        userResumeScore: 100,
+        userSkillMatch: 100,
+        userSkill: 'Senior',
+    },
+    {
+        userName: 'Maxwell',
+        userRole: 'FrontEnd Developer',
+        userResumeScore: 50,
+        userSkillMatch: 60,
+        userSkill: 'Senior',
+    },
+    {
+        userName: 'Pole',
+        userRole: 'BackEnd Developer',
+        userResumeScore: 80,
+        userSkillMatch: 76,
+        userSkill: 'Senior',
+    },
+  ]
+  setPageData();
+}
+
+/**
+ * 1. totalCount 세팅  
+ *    - tableData가 undefined가 아니면 길이만큼, undefined라면 0을 저장  
+ *    - 사실 ts에의해 undefined나 null은 human할당 할 수 없음
+ * 2. totalPage 세팅  
+ *    - 전체 카운트 / 한 페이지당 나타낼 데이터 개수  
+ *    - Math.ceil()을 통해 올림 (크거나 같은 숫자중 가장 작은숫자 즉, 강제 올림)  
+ *       - 0.9 => 1 | 0.4 => 1 
+ *       - 0이 아닐경우 해당 값을, 0일경우 1을 저장
+ *       - 만약 0이면 "조회 가능한 데이터가 없습니다." UI 발생 
+ *          - 페이지네이션 숫자는 1로 출력
+ */
+const setPageData = () => {
+  totalCount.value = tableData.value !== undefined ? tableData.value.length : 0;
+  totalPage.value = Math.ceil(totalCount.value / limit.value) !== 0 ? Math.ceil(totalCount.value / limit.value) : 1
+  tableData.value = dissemble(selectedPage.value - 1, tableData.value, limit.value)
+}
+
+/**
+ * tableData 길이만큼 loop를 돌며 반복.  
+ * size(한 페이지당 나타낼 데이터 개수 `limit`) 만큼 자른 결과 배열을 res 배열에 push.  
+ * push로 쌓인 res에서 현재 선택된 페이지에 해당하는 데이터 반환
+ * 
+ * 
+ * ```js
+ * res.push(data.slice(i, i + size));
+ * ```
+ * 위 코드에 의해 targetData(data)가 11개일 경우가 있을 경우, limit(size) 값이 10이기 때문에  
+ * data.slice(i, i+10) = 10개 / 1개 (총11개 2페이지) 로 쪼개진다.
+ * 
+ * 
+ * @param index selectedPage(ref) - 1
+ * @param data tableData(ref)
+ * @param size limit(ref)
+ * @return 2차원 배열
+ */
+const dissemble = (index: number, data: List[], size: number) => {
+  const res = new Array();
+
+  for(let i = 0; i < data.length; i += size) {
+    res.push(data.slice(i, i + size));
+  }
+  console.log(res)
+  return res[index]
+}
+
+const reset = (pageIdx: number) => {
+  if (pageIdx === 0) selectedPage.value = 1;
+  else selectedPage.value = pageIdx;
+}
+
+watch(selectedPage, (n, o) => {
+  getData();
+})
+
 </script>
 <style lang="scss" scoped>
 .notice {
